b99602ff5f15c46bc7c23637375db536bf77fd6d
Lucas pimentel <lucas47b@hotmail.com>
2025-05-29 09:33:01 -0300

    pagina 4

---
+++ musica4.js
@@ -0,0 +1,305 @@
+function ingles2(){
+    let idioma = document.getElementById("tradu");
+    let ingles = `
+    <pre><code>
+    Key: <span style="color: red;">A</span>
+
+[Intro]  
+<span style="color: red;">A9  E  Bm  D</span>  
+<span style="color: red;">A9  E  Bm</span>  
+<span style="color: red;">A9  E  Bm  D</span>  
+<span style="color: red;">G  D/F#  A9</span>
+
+[Tab - Intro Solo]  
+E|-12b14r12-12--12------9-----------------------------|  
+B|------------------9h10------9-12------10p9-----------|  
+G|-------------------------11------9-11------9---9/10-|  
+D|---------------------------------------------9-------|  
+A|-----------------------------------------------------|  
+E|-----------------------------------------------------|
+
+[Chorus]  
+<span style="color: red;">A9  A9/C#</span>  
+Because He lives  
+<span style="color: red;">D</span>  
+I can face tomorrow  
+<span style="color: red;">A9</span>  
+Because He lives  
+<span style="color: red;">E</span>  
+All fear is gone  
+
+<span style="color: red;">A9</span>  
+Because I know, I know  
+<span style="color: red;">D</span>  
+He holds the future  
+<span style="color: red;">A9</span>           <span style="color: red;">E</span>  
+And life is worth the living  
+<span style="color: red;">A9</span>  
+Just because He lives
+
+[Verse 1]  
+<span style="color: red;">D</span>  
+God sent His Son, they called Him Jesus  
+<span style="color: red;">A9</span>             <span style="color: red;">Bm  E</span>  
+He came to love, heal and forgive  
+<span style="color: red;">A9</span>            <span style="color: red;">D</span>  
+He lived and died to buy my pardon  
+<span style="color: red;">Bm  A9/C#  F#m</span>           <span style="color: red;">E/G#</span>  
+An empty grave is there to prove my Savior lives  
+<span style="color: red;">D</span>  
+Because He lives
+
+[Chorus]  
+<span style="color: red;">A9</span>  
+Because He lives  
+<span style="color: red;">D</span>  
+I can face tomorrow  
+<span style="color: red;">A9</span>  
+Because He lives  
+<span style="color: red;">E</span>  
+All fear is gone  
+
+<span style="color: red;">A9</span>  
+Because I know, I know  
+<span style="color: red;">D</span>  
+He holds the future  
+<span style="color: red;">Bm  A/C#  F#m</span>        <span style="color: red;">E/G#</span>  
+And life is worth the living  
+<span style="color: red;">G  F#5  G#5  E</span>  
+Just because He lives
+
+[Verse 2]  
+<span style="color: red;">A9</span>           <span style="color: red;">D</span>  
+And then one day, I’ll cross the river  
+<span style="color: red;">A9</span>            <span style="color: red;">Bm  E</span>  
+I’ll fight life’s final war with pain  
+<span style="color: red;">A9</span>           <span style="color: red;">D</span>  
+And then as death gives way to victory  
+<span style="color: red;">Bm  A9/C#  F#m</span>         <span style="color: red;">E/G#</span>  
+I’ll see the lights of glory and I’ll know He lives  
+
+[Chorus]  
+<span style="color: red;">A9</span>  
+Because He lives  
+<span style="color: red;">D</span>  
+I can face tomorrow  
+<span style="color: red;">A9</span>  
+Because He lives  
+<span style="color: red;">E</span>  
+All fear is gone  
+
+<span style="color: red;">A9</span>  
+Because I know, I know  
+<span style="color: red;">D</span>  
+He holds the future  
+<span style="color: red;">Bm  A/C#  F#m</span>        <span style="color: red;">E/G#</span>  
+And life is worth the living  
+<span style="color: red;">G</span>  
+Just because He lives
+
+[Solo]  
+<span style="color: red;">A9  G  G5</span>  
+<span style="color: red;">F#5  A5  E</span>
+
+[Tab - Main Solo]  
+Part 1 of 4  
+E|--------10---------------------10--------------------|  
+B|-10-8-7----7-8-7--7~--10-8-7----7-8-7--7--------|  
+
+Part 2 of 4  
+E|---------12------------------------------------------|  
+B|-12-10-9----9-10-9--9-----------------------------|  
+
+Part 3 of 4  
+B|-12-0-10-12-0-0--14-0-12-14-0-0--15-0-14-15----------|  
+
+Part 4 of 4  
+E|----------17-----------------------------------------|  
+B|-17-15-14----14-15-14--14--15-17b19~--7/87-7/10-|
+
+[Final Chorus]  
+<span style="color: red;">A9</span>  
+Because He lives  
+<span style="color: red;">D</span>  
+I can face tomorrow  
+<span style="color: red;">A9</span>  
+Because He lives  
+<span style="color: red;">E</span>  
+All fear is gone  
+
+<span style="color: red;">A9</span>  
+Because I know, I know  
+<span style="color: red;">D</span>  
+He holds the future  
+<span style="color: red;">Bm  A/C#  F#m</span>        <span style="color: red;">E/G#</span>  
+And life is worth the living  
+<span style="color: red;">D  A9/C#  A9</span>  
+Just because He lives </code></pre>
+`;
+
+    idioma.innerHTML = ingles;
+
+
+}
+
+function portugues2(){
+
+    let idioma = document.getElementById("tradu");
+    let ptBR = `<pre><code>
+        Tom: <span style="color: red;">A</span>
+        [Intro] <span style="color: red;">
+                A9  E  Bm  D
+                A9  E  Bm
+                A9  E  Bm  D
+                G  D/F#  A9
+        </span>
+        
+        [Tab - Solo Intro] 
+        
+        E|-12b14r12-12~--12------9-----------------------------|
+        B|------------------9h10------9-12------10p9-----------|
+        G|-------------------------11------9-11------9---9/10~-|
+        D|---------------------------------------------9-------|
+        A|-----------------------------------------------------|
+        E|-----------------------------------------------------|
+        
+        [Refrão]
+                <span style="color: red;">A9  A9/C#</span>
+        Porque Ele vive
+                  <span style="color: red;">D</span>
+        Posso crer no amanhã
+                <span style="color: red;">A9</span>
+        Porque Ele vive
+               <span style="color: red;">E</span>
+        Temor não há
+                <span style="color: red;">A9</span>
+        Mas eu bem sei, eu sei
+                 <span style="color: red;">D</span>
+        Que a minha vida
+              <span style="color: red;">A9 </span>           E
+        Está nas mãos do meu Jesus
+                <span style="color: red;">A9</span>
+        Que vivo está
+        
+        [Primeira Parte]
+                            <span style="color: red;">D</span>
+        Deus enviou seu Filho amado
+             <span style="color: red;">A9</span>             <span style="color: red;">Bm  E</span>
+        Pra perdoar, pra me salvar
+                <span style="color: red;">A9</span>            <span style="color: red;">D</span>
+        Na cruz morreu por meu pecado
+        Bm      <span style="color: red;">A9/C#  F#m</span>              <span style="color: red;">E/G#</span>
+           Mas ressur_giu e vivo com o Pai está
+                <span style="color: red;">D</span>
+        Porque Ele vive
+        
+        [Refrão]
+                <span style="color: red;">A9</span>
+        Porque Ele vive
+                  <span style="color: red;">D</span>
+        Posso crer no amanhã
+                <span style="color: red;">A9</span>
+        Porque Ele vive
+               <span style="color: red;">E</span>
+        Temor não há
+                <span style="color: red;">A9</span>
+        Mas eu bem sei, eu sei
+                 <span style="color: red;">D</span>
+        Que a minha vida
+        <span style="color: red;">Bm    A9/C#  F#m           E/G#</span>   
+           Está nas mãos do meu Jesus
+                <span style="color: red;">G  F#5  G#5  E</span>
+        Que vivo está
+        
+        [Segunda Parte]
+                <span style="color: red;">A9 </span>           <span style="color: red;">D</span>
+        E quando enfim, chegar a hora
+              <span style="color: red;">A9</span>            <span style="color: red;">Bm  E</span>
+        Em que a morte enfrentarei
+                 <span style="color: red;">A9           D</span>
+        Sem medo, então, terei vitória
+        <span style="color: red;">Bm    A9/C#   F#m             E/G#</span>
+           Verei na Glória, ao meu Jesus
+                <span style="color: red;">D</span>
+        Que vivo está
+        
+        [Refrão]
+                <span style="color: red;">A9</span>
+        Porque Ele vive
+                  <span style="color: red;">D</span>
+        Posso crer no amanhã
+                <span style="color: red;">A9</span>
+        Porque Ele vive
+               <span style="color: red;">E</span>
+        Temor não há
+                <span style="color: red;">A9</span>
+        Mas eu bem sei, eu sei
+                 <span style="color: red;">D</span>
+        Que a minha vida
+        <span style="color: red;">Bm    A/C#   F#m           E/G#</span>   
+           Está nas mãos do meu Jesus
+                <span style="color: red;">G</span>
+        Que vivo está
+        
+        [Solo] <span style="color: red;">
+        A9  G  G5
+               F#5  A5  E
+        </span>
+        
+        [Tab - Solo Principal]
+        
+        Parte 1 de 4 
+        E|--------10---------------------10--------------------|
+        B|-10-8-7----7~-8-7-x-7~--10-8-7----7~-8-7-x-7~--------|
+        G|-----------------------------------------------------|
+        D|-----------------------------------------------------|
+        A|-----------------------------------------------------|
+        E|-----------------------------------------------------|
+        
+        Parte 2 de 4
+        E|---------12------------------------------------------|
+        B|-12-10-9----9~-10-9-x-9~-----------------------------|
+        G|-----------------------------------------------------|
+        D|-----------------------------------------------------|
+        A|-----------------------------------------------------|
+        E|-----------------------------------------------------|
+        
+        Parte 3 de 4 
+        E|-----------------------------------------------------|
+        B|-12-0-10-12-0-0--14-0-12-14-0-0--15-0-14-15----------|
+        G|-----------------------------------------------------|
+        D|-----------------------------------------------------|
+        A|-----------------------------------------------------|
+        E|-----------------------------------------------------|
+        
+        Parte 4 de 4
+        E|----------17-----------------------------------------|
+        B|-17-15-14----14~-15-14-x-14~--15-17b19~--7/87-7/10~-|
+        G|-----------------------------------------------------|
+        D|-----------------------------------------------------|
+        A|-----------------------------------------------------|
+        E|-----------------------------------------------------|
+        
+        [Refrão]
+                <span style="color: red;">A9</span>
+        Porque Ele vive
+                  <span style="color: red;">D</span>
+        Posso crer no amanhã
+                <span style="color: red;">A9</span>
+        Porque Ele vive
+               <span style="color: red;">E</span>
+        Temor não há
+                <span style="color: red;">A9</span>
+        Mas eu bem sei, eu sei
+                 <span style="color: red;">D</span>
+        Que a minha vida
+        <span style="color: red;">Bm    A/C#   F#m           E/G#</span>   
+           Está nas mãos do meu Jesus
+                <span style="color: red;">D  A9/C#  A9</span>
+        Que vivo está
+            </code></pre>
+    `
+
+idioma.innerHTML = ptBR
+
+}
